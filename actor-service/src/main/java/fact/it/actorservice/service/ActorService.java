e6dfe990b3df739b794676c34e2071d12782b8d0
JoppeKerkhofs <r0878799@student.thomasmore.be>
2023-12-27 16:50:59 +0100

    Updated actors to correspond to movies

--- actor-service/src/main/java/fact/it/actorservice/service/ActorService.java
+++ actor-service/src/main/java/fact/it/actorservice/service/ActorService.java
@@ -17,26 +17,17 @@ public class ActorService {
     @PostConstruct
     public void loadData() {
         if (actorRepository.count() == 0) {
-            actorRepository.save(new Actor(1L,"Tom Hanks", Date.valueOf("1956-07-09"), "USA", true));
-            actorRepository.save(new Actor(2L, "Brad Pitt", Date.valueOf("1963-12-18"), "USA", true));
-            actorRepository.save(new Actor(3L, "Morgan Freeman", Date.valueOf("1937-06-01"), "USA", true));
-            actorRepository.save(new Actor(4L, "Jennifer Aniston", Date.valueOf("1969-02-11"), "USA", true));
-            actorRepository.save(new Actor(5L, "Julia Roberts", Date.valueOf("1967-10-28"), "USA", true));
-            actorRepository.save(new Actor(6L, "George Clooney", Date.valueOf("1961-05-06"), "USA", true));
-            actorRepository.save(new Actor(7L, "Scarlett Johansson", Date.valueOf("1984-11-22"), "USA", true));
-            actorRepository.save(new Actor(8L, "Robert Downey Jr.", Date.valueOf("1965-04-04"), "USA", true));
-            actorRepository.save(new Actor(9L, "Will Smith", Date.valueOf("1968-09-25"), "USA", true));
-            actorRepository.save(new Actor(10L, "Johnny Depp", Date.valueOf("1963-06-09"), "USA", true));
-            actorRepository.save(new Actor(11L, "Leonardo DiCaprio", Date.valueOf("1974-11-11"), "USA", true));
-            actorRepository.save(new Actor(12L, "Dwayne Johnson", Date.valueOf("1972-05-02"), "USA", true));
-            actorRepository.save(new Actor(13L, "Matt Damon", Date.valueOf("1970-10-08"), "USA", true));
-            actorRepository.save(new Actor(14L, "Natalie Portman", Date.valueOf("1981-06-09"), "USA", true));
-            actorRepository.save(new Actor(15L, "Emma Watson", Date.valueOf("1990-04-15"), "USA", true));
-            actorRepository.save(new Actor(16L, "Anne Hathaway", Date.valueOf("1982-11-12"), "USA", true));
-            actorRepository.save(new Actor(17L, "Meryl Streep", Date.valueOf("1949-06-22"), "USA", true));
-            actorRepository.save(new Actor(18L, "Angelina Jolie", Date.valueOf("1975-06-04"), "USA", true));
-            actorRepository.save(new Actor(19L, "Emma Stone", Date.valueOf("1988-11-06"), "USA", true));
-            actorRepository.save(new Actor(20L, "Charlize Theron", Date.valueOf("1975-08-07"), "USA", true));
+            // add the following actors: 1L "Donald Glover", 2L "Beyoncé Knowles", 3L "James Earl Jones", 4L Joaquin Phoenix, 5L Robert De Niro, 6L Robert Downey Jr., 7L Chris Evans, 8L Mark Ruffalo 9L, Chris Hemsworth, 10L Scarlett Johansson
+            actorRepository.save(new Actor(1L, "Donald Glover", Date.valueOf("1983-09-25"), "USA", true));
+            actorRepository.save(new Actor(2L, "Beyoncé Knowles", Date.valueOf("1981-09-04"), "USA", true));
+            actorRepository.save(new Actor(3L, "James Earl Jones", Date.valueOf("1931-01-17"), "USA", true));
+            actorRepository.save(new Actor(4L, "Joaquin Phoenix", Date.valueOf("1974-10-28"), "USA", true));
+            actorRepository.save(new Actor(5L, "Robert De Niro", Date.valueOf("1943-08-17"), "USA", true));
+            actorRepository.save(new Actor(6L, "Robert Downey Jr.", Date.valueOf("1965-04-04"), "USA", true));
+            actorRepository.save(new Actor(7L, "Chris Evans", Date.valueOf("1981-06-13"), "USA", true));
+            actorRepository.save(new Actor(8L, "Mark Ruffalo", Date.valueOf("1967-11-22"), "USA", true));
+            actorRepository.save(new Actor(9L, "Chris Hemsworth", Date.valueOf("1983-08-11"), "Australia", true));
+            actorRepository.save(new Actor(10L, "Scarlett Johansson", Date.valueOf("1984-11-22"), "USA", true));
         }
     }
 
