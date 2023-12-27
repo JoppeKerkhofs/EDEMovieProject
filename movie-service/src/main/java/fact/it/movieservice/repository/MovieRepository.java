bc47f45977802aa9596ce6e3fba2347cfe7f7f3f
JoppeKerkhofs <r0878799@student.thomasmore.be>
2023-12-27 17:13:12 +0100

    Added docker compose and update movie

--- movie-service/src/main/java/fact/it/movieservice/repository/MovieRepository.java
+++ movie-service/src/main/java/fact/it/movieservice/repository/MovieRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import fact.it.movieservice.model.Movie;
 
 public interface MovieRepository extends MongoRepository<Movie, String> {
-    List<Movie> findByName(String name);
+    List<Movie> findByName(String title);
 }
